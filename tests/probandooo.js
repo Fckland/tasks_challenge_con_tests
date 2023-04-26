724935ea9325b5d0e1f86445651be319c37e3f32
Fckland <carlosfc.90@hotmail.com>
2023-04-26 15:59:56 -0300

    Prueba (?

---
+++ tests/probandooo.js
@@ -0,0 +1,10 @@
+// write tests for login.js
+
+describe('login.js', () => {
+    it('should return the token if the authentication is successful', () => {
+        const user = {
+            'mail':'test@example.com',
+            'password':'testpassword'
+        }
+    })
+})
